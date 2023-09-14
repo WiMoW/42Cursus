2b08602527c89fb070a039efe41102e7047bba92
WiMo <wildmonkeyjam@gmail.com>
2023-09-14 15:35:15 +0200

    Updated

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -14,14 +14,14 @@
 
 void    ft_bzero(void *s, size_t n)
 {
-    int i;
-    unsigned    char    *ptr;
+	int i;
+	unsigned char    *ptr;
 
-    i = 0;
-    ptr = s;
-    while ((size_t)i < n)
-    {
-        ptr[i] = 0;
-        i++;
-    }
+	i = 0;
+	ptr = s;
+	while ((size_t)i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
 }
--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+	int			i;
+	const char	*string;
+
+	i = 0;
+	string = s;
+	while (i < n)
+	{
+		if (string[i] == c)
+			return ((void *)(string + 1));
+		i++;
+	}
+	return (0);
+}
