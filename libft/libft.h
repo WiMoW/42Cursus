ca05284a1f1672d615516ad89dc128aad76b927b
WiMo <wildmonkeyjam@gmail.com>
2023-09-17 14:01:14 +0200

    Updated header

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: wimo <wimo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 11:36:38 by dacaball          #+#    #+#             */
-/*   Updated: 2023/09/17 12:10:53 by wimo             ###   ########.fr       */
+/*   Updated: 2023/09/17 14:00:43 by wimo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(char *str);
 int		ft_strncmp(char *s1, char *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -36,7 +35,9 @@ void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strchr(char *s, int c);
 char	*ft_strrchr(char *s, int c);
+
 size_t	ft_strlcat(char *dst, char *src, size_t size);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
+size_t	ft_strlen(const char *str);
 
 #endif
