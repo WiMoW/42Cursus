e6d883beef6e112364a8b9aa0a5186e8833b9f23
WiMo <wildmonkeyjam@gmail.com>
2023-10-02 15:26:15 +0200

    Started ft_printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacaball <dacaball@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 13:23:16 by dacaball          #+#    #+#             */
+/*   Updated: 2023/10/02 13:27:55 by dacaball         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(char const *str, ...);
+
+
+
+#endif
