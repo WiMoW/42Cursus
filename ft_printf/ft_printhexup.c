a78f9a62c490f32aea14686ffd63e321162ad879
David Caballero Centeno <wildmonkeyjam@gmail.com>
2023-10-04 13:32:43 +0200

    Updated

---
+++ ft_printf/ft_printhexup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printhexup.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacaball <dacaball@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 13:31:14 by dacaball          #+#    #+#             */
+/*   Updated: 2023/10/04 13:32:09 by dacaball         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_printhexup(unsigned int decimal, int *i)
+{
+	char const	base_hex[] = "0123456789ABCDEF";
+
+	if (decimal > 16)
+	{
+		ft_printhexup(decimal / 16, i);
+	}
+	ft_printchar_fd((char)base_hex[decimal % 16], i);
+}
