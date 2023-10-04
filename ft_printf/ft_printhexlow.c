15c89b4b770a7dc31dfc09d43b2ab637b97bd0a7
David Caballero Centeno <wildmonkeyjam@gmail.com>
2023-10-04 13:30:48 +0200

    Updated

--- ft_printf/ft_printhexlow.c
+++ ft_printf/ft_printhexlow.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printhexlow.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dacaball <dacaball@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dacaball <dacaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 21:12:46 by dacaball          #+#    #+#             */
-/*   Updated: 2023/10/04 11:16:23 by dacaball         ###   ########.fr       */
+/*   Updated: 2023/10/04 13:30:01 by dacaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_printhexlow(unsigned int	decimal, int *i)
+void	ft_printhexlow(unsigned int decimal, int *i)
 {
-    char const	base_hex[] = "0123456789abcdef";
+	char const	base_hex[] = "0123456789abcdef";
+
 	if (decimal > 16)
 	{
 		ft_printhexlow(decimal / 16, i);
